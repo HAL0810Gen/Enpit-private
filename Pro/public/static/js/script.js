94e1a54c57035a63fe6dc5e136c63541884fb7fa
Hal 9000 <genryuk@gmail.com>
2026-09-17 14:18:22 +0900

    jfaoiehfa

--- Pro/public/static/js/script.js
+++ Pro/public/static/js/script.js
@@ -1,24 +1,81 @@
-const onClick = (type) => {
-    addsclist(type);
-};
+/* main.js */
 
-const addsclist = (type) => {
-    const now = new Date();
-    const month = String(now.getMonth() + 1).padStart(2, '0'); // 月 (01〜12)
-    const day = String(now.getDate()).padStart(2, '0');       // 日 (01〜31)
-    const hours = String(now.getHours()).padStart(2, '0');
-    const minutes = String(now.getMinutes()).padStart(2, '0');
+/*
+ * グローバル変数
+ */
+var no;				// 数値格納用
+var number;		// 数値表示部分のDOM取得用						
 
-    const li = document.createElement("li");
-    const div = document.createElement("div");
-    div.className = "sclist";
+/*
+ * スワイプイベント設定
+ */
+function setSwipe(elem) {
+	let t = document.querySelector(elem);
+	let startX;		// タッチ開始 x座標
+	let startY;		// タッチ開始 y座標
+	let moveX;	// スワイプ中の x座標
+	let moveY;	// スワイプ中の y座標
+	let dist = 30;	// スワイプを感知する最低距離（ピクセル単位）
+	
+	// タッチ開始時： xy座標を取得
+	t.addEventListener("touchstart", function(e) {
+		e.preventDefault();
+		startX = e.touches[0].pageX;
+		startY = e.touches[0].pageY;
+	});
+	
+	// スワイプ中： xy座標を取得
+	t.addEventListener("touchmove", function(e) {
+		e.preventDefault();
+		moveX = e.changedTouches[0].pageX;
+		moveY = e.changedTouches[0].pageY;
+	});
+	
+	// タッチ終了時： スワイプした距離から左右どちらにスワイプしたかを判定する/距離が短い場合何もしない
+	t.addEventListener("touchend", function(e) {
+		if (startX > moveX && startX > moveX + dist) {		// 右から左にスワイプ
+			previous();
+		}
+		else if (startX < moveX && startX + dist < moveX) {	// 左から右にスワイプ
+			next();
+		}
+	});
+}
 
-    const p = document.createElement("p");
-    // 表示形式を「MM月DD日 hh:mm:ss」にする例
-    p.innerText = `${type}予定：${month}月${day}日${hours}:${minutes}`;
+/*
+ * 次の番号を表示
+ */
+function next(){
+	no ++;
+	setNumber();
+}
 
-    div.appendChild(p);
-    li.appendChild(div);
+/*
+ * 前の番号を表示
+ */
+function previous(){
+	no --;
+	setNumber();
+}
 
-    document.getElementById("sclist").appendChild(li);
-};
+/*
+ * 数値を画面に表示する
+ */
+function setNumber(){
+	number.innerHTML = no;
+}
+
+/*
+ * 起動時の処理
+ */
+window.addEventListener("load", function(){
+	// 数値表示部分のDOM取得
+	number = document.getElementById("number");
+	
+	// 数値を画面に表示
+	no = 0;
+	setNumber();
+
+	// スワイプイベント設定
+	setSwipe("#contents");
+});
